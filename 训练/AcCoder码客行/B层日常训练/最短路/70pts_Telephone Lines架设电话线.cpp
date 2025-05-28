632368602aae2ce0f1fcfe26c3f9191cb55b44ba
Misaka2298 <qzj2183221872@163.com>
2025-05-29 00:27:57 +0800

    2025/05/29 周四 的居家日常训练

---
+++ 训练/AcCoder码客行/B层日常训练/最短路/70pts_Telephone Lines架设电话线.cpp
@@ -0,0 +1,73 @@
+#include <bits/stdc++.h>
+using namespace std;
+const int maxn = 2e5+10;
+#define endl '\n';
+typedef pair<int, int> PII;
+
+int n, m, k;
+int e[maxn], ne[maxn], h[maxn], w[maxn], idx;
+int dist[maxn];
+bool st[maxn];
+queue<int>q;
+
+void addedge(int a, int b, int c)
+{
+	e[idx] = b, ne[idx] = h[a], w[idx] = c, h[a] = idx++;
+}
+
+bool isLessthanK(int check)
+{
+	memset(dist, 0x3f, sizeof dist);
+	if(q.size()) q.pop();
+	q.push(1);
+	memset(st, false, sizeof st);
+	dist[1] = 0;
+	
+	while(q.size())
+	{
+		int t = q.front(); q.pop();
+		if(st[t]) continue;
+		st[t] = 1;
+		
+		for(int i = h[t] ; i != -1 ; i = ne[i])
+		{
+			int j = e[i];
+			int cpyw = w[i];
+			if(cpyw <= check) cpyw = 0; 
+			else cpyw = 1;
+			if(dist[j] > dist[t] + cpyw)
+			{
+				dist[j] = dist[t] + cpyw;
+				q.push(j); 
+			}
+		}
+	}
+	
+	return dist[n] <= k;
+}
+
+signed main()
+{
+	cin >> n >> m >> k;
+	int l = 1, r = 0;
+	memset(h, -1, sizeof h);
+	for(int i = 0 ; i < m ; i++)
+	{
+		int a, b, c;
+		cin >> a >> b >> c;
+		addedge(a, b, c); addedge(b, a, c);
+		r += c;
+	}
+	
+	while(l <= r)
+	{
+		int mid = (l+r)/2;
+		if(isLessthanK(mid))
+			r = mid - 1;
+		else 
+			l = mid + 1;
+	}
+	
+	if (l > 1e6) cout << -1;
+	else cout << l;
+}
